5a7a9594165a565a63aa9e24e81e511a54d69487
Doidjin <leocabbau2@gmail.com>
2023-06-17 10:30:13 -0300

    feat: initial commit for ci in Go

---
+++ math_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func SomaTest(t *testing.T) {
+
+	total := Soma(10, 10)
+
+	if total != 20 {
+		t.Errorf("Resultado da soma é inválido, Resultado: %d. Esperado: %d", total, 20)
+	}
+}
